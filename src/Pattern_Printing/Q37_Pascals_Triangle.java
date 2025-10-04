5a2a7cff0dd338bcf13ea32b30cba661dac388ee
Shivam Agrawal <rkgshivam1@gmail.com>
2025-10-04 18:26:24 +0530

    Q1 to Q45 of Pattern Printing

---
+++ src/Pattern_Printing/Q37_Pascals_Triangle.java
@@ -0,0 +1,20 @@
+package Pattern_Printing;
+import java.util.Scanner;
+public class Q37_Pascals_Triangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            // Pyramid Structure Type-2
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            int x = 1; // every line starts with 1
+            for(int j=1; j<=i; j++){
+                System.out.print(x+" ");
+                x = x* (i-j)/(j); // (next term) = (previous term)*((i-j_previous)/j)  --> Less Intutive , remember this
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Pattern_Printing/Q38_Reverse_Number_Pyramid.java
@@ -0,0 +1,19 @@
+package Pattern_Printing;
+import java.util.Scanner;
+public class Q38_Reverse_Number_Pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=i-1; j>=1; j--){ // for spaces
+                System.out.print(" ");
+            }
+            int a = i;
+            for(int j=1; j<=n+1-i; j++){ // right triangle structure
+                System.out.print(a+" ");
+                a++;
+            }
+            System.out.println();
+        }
+    }
+}
